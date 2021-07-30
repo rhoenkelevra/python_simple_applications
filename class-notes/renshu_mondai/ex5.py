68c2f87ca46159c636a780f8bd9e6e45d7d78dbf
Rhoen Rene <rhoenrene@me.com>
2021-07-31 01:34:16 +0900

    put question from the pdf to the exercise file

--- class-notes/renshu_mondai/ex5.py
+++ class-notes/renshu_mondai/ex5.py
@@ -1,8 +1,16 @@
 # -*- coding: utf-8 -*-
 """
-Created on Fri Jul 30 15:00:04 2021
+演習5
+■ 仕様 
+以下のアスキーアートを表示するコンソールプログラムを作成してください。
+■ 実行画面
+○○○ 
+○○ 
+○
 
-@author: user24
+○ 
+○○ 
+○○○
 """
 
 # 変数
@@ -14,24 +22,23 @@ ball_cnt = 3
 counter = 0
 
 # 7行を印刷する為　
-while counter <= 7 :
-     # ボールかける　ボールカウンター変数を印刷するする　  
+while counter <= 7:
+    # ボールかける　ボールカウンター変数を印刷するする
     print(ball * ball_cnt)
     # ボールカウンタを　ー＝１
     ball_cnt -= 1
     # 　カウンター変数　＋＝１
-    counter +=1
-    
+    counter += 1
+
     # ボールカウンタ０になったら
     if ball_cnt == 0:
         # 空行を含める
         counter += 1
-        
+
         # ボールカウンター変数を３までループ
         while ball_cnt <= 3:
-            # ボールかける　ボールカウンター変数を印刷するする　 
+            # ボールかける　ボールカウンター変数を印刷するする
             print(ball * ball_cnt)
             # ボールカウンター変数をＵＰする
             ball_cnt += 1
             counter += 1
-    
