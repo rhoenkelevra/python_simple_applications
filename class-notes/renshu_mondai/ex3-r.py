74de5248ee3e00fc2c655168d4d2c202985d4ba1
Rhoen Rene <rhoenrene@me.com>
2021-08-04 00:31:36 +0900

    file reorganized and pyenv venv added

--- class-notes/renshu_mondai/ex3-r.py
+++ class-notes/renshu_mondai/ex3-r.py
@@ -1,4 +1,6 @@
 num = int(input("数値を入力してください。："))
+
+
 for i in range(300):
-    if i%num == 0:
+    if i % num == 0:
         print(i)
--- class-notes/renshu_mondai/ex7-r.py
+++ class-notes/renshu_mondai/ex7-r.py
@@ -1,17 +1,18 @@
 import math
 
+
 r = float(input("半径を入力してください。："))
 
-#円の面積 circle_S
+# 円の面積 circle_S
 circle_S = math.pi*r*r
 
-#円周 L
-L=2*math.pi*r
+# 円周 L
+L = 2*math.pi*r
 
-#球の体積 V
+# 球の体積 V
 V = 4/3*math.pi*r**3
 
-#球の表面積 S 
+# 球の表面積 S
 S = 4*math.pi*r**2
 
 print("円の面積："+str(round(circle_S, 2)))
